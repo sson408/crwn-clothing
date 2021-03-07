7d97fa6d6a5e96d7389957db25e86e03e018591c
SeanS <sson408@users.noreply.github.com>
2021-03-07 23:21:26 +1300

    broken our homepage into small components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import HomePage from './homepage.compontent'
+import HomePage from './pages/homepage/homepage.component'
 
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+import Directory from '../../componenet/directory/directory.cpmponent'
+
+
+
+const HomePage = () => (
+    <div className='homepage'>
+      <Directory  />
+    </div>
+)
+
+export default HomePage
